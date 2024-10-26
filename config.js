c7928ba6305376f34face1f5f9da72c09be6a8a0
D3AT123 <kengice5@gmail.com>
2024-10-27 03:02:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,12 +39,12 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "lavalink.alfari.id",
-        port: 443,
-        password: "catfein",
+        host: "lavalink01.techbyte.host",
+        port: 2036,
+        password: "NAIGLAVA-dash.techbyte.host",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
-        secure: true
+        secure: false
     },
  ]
 }
